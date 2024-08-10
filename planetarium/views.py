06dabf0c98a3bbe23aa22430e8225904cbff5c56
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-10 15:21:26 +0300

    Create views

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import (
+    PlanetariumDome,
+    ShowTheme,
+    AstronomyShow,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
+
+from planetarium.serializers import (
+    PlanetariumDomeSerializer,
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+)
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
