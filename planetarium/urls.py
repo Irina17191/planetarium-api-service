aa4f574364ca5f18502a5a3348f93994b7b463a3
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-10 23:04:48 +0300

    Refactor urls for planitarium app

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -11,10 +11,10 @@ from planetarium.views import (
 
 router = routers.DefaultRouter()
 router.register("planetarium_domes", PlanetariumDomeViewSet)
-router.register("show_theme", ShowThemeViewSet)
-router.register("astronomy_show", AstronomyShowViewSet)
-router.register("show_session", ShowSessionViewSet)
-router.register("reservation", ReservationViewSet)
+router.register("show_themes", ShowThemeViewSet)
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
