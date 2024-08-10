fcf16a07ccfb241fadbeba83246d6a8a3ebd37d0
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-10 15:22:13 +0300

    Create urls for planetarium app

---
+++ planetarium/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from planetarium.views import (
+    PlanetariumDomeViewSet,
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("planetarium_domes", PlanetariumDomeViewSet)
+router.register("show_theme", ShowThemeViewSet)
+router.register("astronomy_show", AstronomyShowViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "planetarium"
