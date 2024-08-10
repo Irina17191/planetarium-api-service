88f12225858378854f17101bbf3ca36a7d6058ea
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-10 15:38:52 +0300

    Install Django Debug Toolbar

--- planetarium_api_service/urls.py
+++ planetarium_api_service/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
