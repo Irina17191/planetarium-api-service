85b55fcec42d7fd643e93e3df74fdc508667afe1
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-22 20:50:46 +0300

    Set throttling policy

--- planetarium_api_service/settings.py
+++ planetarium_api_service/settings.py
@@ -149,7 +149,13 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 8,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
+
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Planetarium Service API",
